31e6a52acf8c9d5df1cdda65d169cb898acf72fd
June "A4B" Huh <apple4banana@gmail.com>
2024-12-25 23:50:59 +0900

    update jsdoc and deno.json

--- main.ts
+++ main.ts
@@ -4,5 +4,12 @@
 * @module A4BShuffler
 */
 
+/**
+ * Interface for A4B Shuffler instance
+ */
 export { type IA4BShuffler } from './src/types.ts';
+
+/**
+ * Create text shuffling animation controller
+ */
 export { default as A4BShuffler } from './src/a4b.shuffler.ts';
